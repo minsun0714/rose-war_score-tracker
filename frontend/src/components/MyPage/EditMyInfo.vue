e9417937b4df541d973ae3df4f9033eea6509303
이민선 <woooooooow22@gmail.com>
2024-10-31 17:38:42 +0900

    feat: 사진 업로드 기능 추가

--- frontend/src/components/MyPage/EditMyInfo.vue
+++ frontend/src/components/MyPage/EditMyInfo.vue
@@ -2,19 +2,57 @@
 import { MyInfoKey } from '@/lib/types'
 import ProfilePic from '../../assets/Male User.svg'
 import SignatureBtn from '../common/SignatureBtn.vue'
+import { ref } from 'vue'
+
 const myInfo = {
   profilePic: ProfilePic,
   name: '이민선',
   id: 'minsun',
   nickname: 'minsun',
 }
+
+const imageFile = ref<File | null>(null)
+const imageUrl = ref<string>(ProfilePic) // 이미지 URL 상태 추가
+
+const handleFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  if (target.files) {
+    imageFile.value = target.files[0]
+    imageUrl.value = URL.createObjectURL(imageFile.value) // 선택한 파일의 URL 생성
+  }
+}
 </script>
 
 <template>
-  수정 페이지 입니다.
   <div class="flex flex-col justify-between items-start py-6">
-    <div class="flex flex-row justify-center gap-2">
-      <SignatureBtn text="취소" /> <SignatureBtn text="완료" />
+    <div class="flex flex-col justify-center items-center border w-full">
+      <label>프로필 사진</label>
+      <div class="mt-2">
+        <img
+          :src="imageUrl"
+          alt="미리보기"
+          class="w-8 h-8 object-cover rounded-full"
+        />
+      </div>
+      <!-- 파일 입력 숨기기 -->
+      <input
+        type="file"
+        @change="handleFileChange"
+        class="hidden"
+        id="fileInput"
+      />
+      <label
+        for="fileInput"
+        class="w-full cursor-pointer text-center underline text-slate-400 text-sm"
+      >
+        파일 선택
+      </label>
+      <span v-if="imageFile?.name" class="mt-1 text-sm text-gray-600">{{
+        imageFile?.name
+      }}</span>
+    </div>
+    <div class="flex flex-row justify-center gap-2 mt-4">
+      <SignatureBtn text="완료" />
     </div>
     <p class="text-sm text-slate-500">회원탈퇴</p>
   </div>
