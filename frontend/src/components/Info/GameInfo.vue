088ba3b25681dffe05e64a04b9538850842dc4e2
이민선 <woooooooow22@gmail.com>
2024-10-31 14:13:34 +0900

    feat: my page 내 게임 정보 생성

---
+++ frontend/src/components/Info/GameInfo.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { gameInfoKey } from '@/lib/types'
+
+const gameInfo = {
+  rank: 1,
+  maxScore: 500,
+  winCount: 150,
+  loseCount: 150,
+  drawCount: 46,
+  playCount: 346,
+}
+</script>
+
+<template>
+  <div class="flex justify-center flex-col border p-4">
+    <ul class="grid grid-cols-2 gap-y-4">
+      <template v-for="(value, key) in gameInfo" :key="key">
+        <li class="text-slate-500 text-sm text-center">
+          {{ gameInfoKey[key] }}
+        </li>
+        <li class="text-center">{{ value }}</li>
+      </template>
+    </ul>
+  </div>
+</template>
---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,8 @@
+export enum gameInfoKey {
+  rank = '등수',
+  maxScore = '최고 점수',
+  winCount = '승',
+  loseCount = '패',
+  drawCount = '무',
+  playCount = 'play 횟수',
+}
