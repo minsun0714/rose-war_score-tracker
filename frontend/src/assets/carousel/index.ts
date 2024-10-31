f74e9035c02630974c2c7a6381baf77a8605ce54
이민선 <woooooooow22@gmail.com>
2024-10-31 11:58:34 +0900

    refactor: 바렐 패턴 적용

---
+++ frontend/src/assets/carousel/index.ts
@@ -0,0 +1,6 @@
+import RoseWar1 from './rose war1.jpg'
+import RoseWar2 from './rose war2.jpg'
+import RoseWar3 from './rose war3.jpg'
+
+const carouselItems = { RoseWar1, RoseWar2, RoseWar3 }
+export default carouselItems
