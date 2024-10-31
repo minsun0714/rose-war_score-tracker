9cebc191f3b17c91043eb1faa3e0e14ce91081f9
이민선 <woooooooow22@gmail.com>
2024-10-31 20:36:57 +0900

    feat: 게임 결과를 보드판에 클릭하는 기능 추가

---
+++ frontend/src/components/Play/PlayBoard.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import ProfileImg from '../../assets/Male User.svg'
+import SignatureBtn from '../common/SignatureBtn.vue'
+
+const user1 = { profileImg: ProfileImg, nickname: 'minsun' }
+const user2 = { profileImg: ProfileImg, nickname: 'kangmin' }
+
+const board = ref(
+  Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => 0)),
+)
+
+const turn = ref<1 | 2>(1)
+
+const changeTurn = (user: 1 | 2) => {
+  turn.value = user
+}
+
+const clickCell = (user: 1 | 2, rowIdx: number, colIdx: number) => {
+  board.value[rowIdx][colIdx] = board.value[rowIdx][colIdx] == user ? 0 : user
+}
+</script>
+
+<template>
+  <div class="flex flex-col justify-center">
+    <h1 class="text-center h-12 flex items-center justify-center">
+      tab cells!
+    </h1>
+
+    <button
+      @click="changeTurn(1)"
+      :class="{
+        'bg-red-500 text-white': turn === 1,
+        'bg-white': turn !== 1,
+      }"
+      class="flex justify-center items-center h-12 gap-2 border border-slate-700"
+    >
+      <img
+        :src="user1.profileImg"
+        alt=""
+        class="border border-white rounded-full h-6 w-6"
+      />
+      {{ user1.nickname }}
+    </button>
+
+    <ul class="flex bg-purple gap-y-1 p-2 justify-center">
+      <li v-for="(row, rowIndex) in board" :key="rowIndex">
+        <span
+          v-for="(cell, colIndex) in row"
+          :key="colIndex"
+          @click="clickCell(turn, rowIndex, colIndex)"
+          :class="{
+            'bg-red-500': cell === 1,
+            'bg-yellow-400': cell === 2,
+            'bg-white': cell === 0,
+          }"
+          class="w-6 h-6 border border-gray-300 flex items-center justify-center text-transparent"
+        >
+          {{ cell }}
+        </span>
+      </li>
+    </ul>
+
+    <button
+      @click="changeTurn(2)"
+      :class="{
+        'bg-yellow-400 text-white': turn === 2,
+        'bg-white': turn !== 2,
+      }"
+      class="flex justify-center items-center h-12 gap-2 border border-slate-700"
+    >
+      <img
+        :src="user2.profileImg"
+        alt=""
+        class="border border-white rounded-full h-6 w-6"
+      />
+      {{ user2.nickname }}
+    </button>
+  </div>
+  <div class="h-24 flex justify-end items-center">
+    <SignatureBtn text="결과 확인" />
+  </div>
+</template>
