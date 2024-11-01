959eb72166bcf0c01977b33a5c1ec0c568fd4019
이민선 <woooooooow22@gmail.com>
2024-11-01 19:32:28 +0900

    feat: 상세페이지로 가는 라우트 추가

--- frontend/src/components/Board/PostList.vue
+++ frontend/src/components/Board/PostList.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import PostCard from '../common/PostCard.vue'
 import Search from '../../assets/Search.svg'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const cardData = [
   {
+    id: 0,
     writer: 'minsun',
     title: '이민선이라고 합니다. 잘 부탁드립니다.',
     createdAt: new Date(),
@@ -11,6 +15,7 @@ const cardData = [
     commentCount: 0,
   },
   {
+    id: 1,
     writer: 'minsun',
     title: '또 다른 게시물입니다. 잘 부탁드립니다.',
     createdAt: new Date(),
@@ -23,11 +28,19 @@ const cardData = [
 <template>
   <div>
     <div class="p-4 flex flex-row">
-      <img :src="Search" alt="">
-      <input type="text" placeholder="검색어를 입력해주세요." class="border-b w-full p-2" />
+      <img :src="Search" alt="" />
+      <input
+        type="text"
+        placeholder="검색어를 입력해주세요."
+        class="border-b w-full p-2"
+      />
     </div>
     <ul class="p-4 flex flex-col gap-y-4">
-      <li v-for="(card, index) in cardData" :key="index">
+      <li
+        v-for="card in cardData"
+        :key="card.id"
+        @click="router.push(`/board/${card.id}`)"
+      >
         <PostCard :card="card" />
       </li>
     </ul>
