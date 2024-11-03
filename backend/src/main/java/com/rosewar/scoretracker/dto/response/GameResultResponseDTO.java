cef6d75d4e1dc6202dc7b87aedff6fd5ed86e6a6
이민선 <woooooooow22@gmail.com>
2024-11-03 21:16:05 +0900

    feat: 불필요한 필드 삭제

--- backend/src/main/java/com/rosewar/scoretracker/dto/response/GameResultResponseDTO.java
+++ backend/src/main/java/com/rosewar/scoretracker/dto/response/GameResultResponseDTO.java
@@ -16,7 +16,4 @@ public class GameResultResponseDTO {
     private UserInfoDTO player2; // Player 2의 사용자 ID
     private int score1;
     private int score2;
-
-    private String winnerId;
-    private boolean isDraw;
 }
