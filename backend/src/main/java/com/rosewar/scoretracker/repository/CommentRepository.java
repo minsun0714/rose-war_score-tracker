e55b205a8c7a0e8a40247402ba86c6e7375e5daa
이민선 <woooooooow22@gmail.com>
2024-11-03 15:04:36 +0900

    feat: 댓글의 기본키가 아닌 postId로 찾아야 하므로 메서드 추가

--- backend/src/main/java/com/rosewar/scoretracker/repository/CommentRepository.java
+++ backend/src/main/java/com/rosewar/scoretracker/repository/CommentRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    List<Comment> findByPostId(Long postId); // 특정 게시물의 모든 댓글 조회
     // 특정 부모 댓글에 연결된 모든 대댓글 조회
     List<Comment> findByParent(Comment parent);
 }
