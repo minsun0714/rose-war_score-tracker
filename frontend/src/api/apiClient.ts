decf97110688180b9a0c1d2c970bef78b0d5d38c
이민선 <woooooooow22@gmail.com>
2024-11-04 17:53:50 +0900

    fix: 환경변수 가져올 떄 Vite로 가져오는 방식으로 변경

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 // 요청 Interceptor
 api.interceptors.request.use(
-    // (config) => {
-    //     const token = localStorage.getItem('token');
-    //     if (token) {
-    //         config.headers.Authorization = `Bearer ${token}`;
-    //     }
-    //     return config;
-    // },
+    (config) => {
+        // const token = localStorage.getItem('token');
+        // if (token) {
+        //     config.headers.Authorization = `Bearer ${token}`;
+        // }
+        return config;
+    },
     (error) => {
         return Promise.reject(error);
     }
