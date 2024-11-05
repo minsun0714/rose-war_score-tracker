61ef8f9a46c14f9a81192bbf5c42c801613b1b3f
이민선 <woooooooow22@gmail.com>
2024-11-05 14:37:02 +0900

    feat: api 연결 후 nickname으로 변경

--- frontend/src/components/common/PostCard.vue
+++ frontend/src/components/common/PostCard.vue
@@ -10,7 +10,7 @@ const { card } = defineProps<{
 <template>
   <div class="bg-slate-100 p-4 text-sm">
     <div class="flex flex-row items-center gap-x-2">
-      <p class="text-gray-600">{{ '@' + card.writer }}</p>
+      <p class="text-gray-600">{{ '@' + card.writer.nickname }}</p>
       <h2
         class="font-bold text-sm overflow-hidden text-ellipsis whitespace-nowrap"
       >
@@ -18,7 +18,7 @@ const { card } = defineProps<{
       </h2>
     </div>
     <div class="flex justify-start items-center text-gray-500">
-      <p>{{ card.createdAt.getSeconds() }}분전</p>
+      <p>{{ card.createdAt }}분전</p>
       <span class="flex items-center">
         <img :src="Like" alt="Like" class="w-5 h-5" />
         {{ card.likeCount }}
