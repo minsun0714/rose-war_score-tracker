b3d33f39e60786560497c355d42604608812d90a
이민선 <woooooooow22@gmail.com>
2024-11-06 17:35:08 +0900

    feat: 게임 결과 렌더링

--- frontend/src/api/apiFacade/GameResultApiFacade.ts
+++ frontend/src/api/apiFacade/GameResultApiFacade.ts
@@ -6,8 +6,8 @@ class GameResultApiFacade {
     // 게임 결과 관련 메서드
     static useCreateGameResult() {
       return useMutation({
-        mutationFn: ({ player1Id, player2Id, gameBoard }: GameResultRequest) => GameResultService.createGameResult(player1Id, player2Id, gameBoard),
-        onSuccess: (response) => alert(response.score1 + response.score2),
+        mutationFn: ({ player1Id, player2Id, gameBoard }: GameResultRequest) =>
+          GameResultService.createGameResult(player1Id, player2Id, gameBoard),
         onError: (error) => console.error(error)
       });
     }
