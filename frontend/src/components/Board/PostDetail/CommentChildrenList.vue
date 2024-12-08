7df7f40a31e9ad3cc73e82e21773445d51e889ab
이민선 <woooooooow22@gmail.com>
2024-12-08 17:10:37 +0900

    feat: 대댓글 수정, 삭제 구현

--- frontend/src/components/Board/PostDetail/CommentChildrenList.vue
+++ frontend/src/components/Board/PostDetail/CommentChildrenList.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-import type { CommentResponse } from '@/api/interface/response';
+import type { CommentResponse } from '@/api/interface/response'
 import CurvedArrow from '@/assets/Curved Arrow.svg'
-import { formatDistanceToNow } from 'date-fns';
-import { ko } from 'date-fns/locale';
+import { formatDistanceToNow } from 'date-fns'
+import { ko } from 'date-fns/locale'
 import ProfileImg from '@/assets/Male User.svg'
-
+import { useModifyCommentStore } from '@/stores/modifyComment'
+import CommentUpdateForm from '../../common/CommentUpdateForm.vue'
+import CommentDeleteBtn from './CommentDeleteBtn.vue'
 
 const { commentChildren } = defineProps<{
   commentChildren?: CommentResponse[]
 }>()
+
+const commentToggleStore = useModifyCommentStore()
 </script>
 
 <template>
@@ -29,17 +33,38 @@ const { commentChildren } = defineProps<{
       </span>
       <span class="text-xs w-full flex flex-col gap-2">
         <span class="text-end text-slate-500">{{
-            formatDistanceToNow(new Date(childComment.createdAt), {
-              addSuffix: true,
-              locale: ko,
-            })
-          }}</span>
-        <p class="text-xs flex items-center">
-          {{ childComment.content }}
-        </p>
-        <span class="text-slate-400 flex justify-end gap-1"
-          ><button>수정</button> <button>삭제</button></span
+          formatDistanceToNow(new Date(childComment.createdAt), {
+            addSuffix: true,
+            locale: ko,
+          })
+        }}</span>
+        <template
+          v-if="commentToggleStore.isModifying.has(childComment.commentId)"
         >
+          <CommentUpdateForm
+            :post-id="childComment.postId"
+            :comment-id="childComment.commentId"
+            :content="childComment.content"
+            :toggle-modify-button="commentToggleStore.toggleModifyBtn"
+          />
+        </template>
+        <template v-else>
+          <p class="text-xs flex items-center">{{ childComment.content }}</p>
+        </template>
+
+        <span class="text-slate-400 flex justify-end gap-1"
+          ><button
+            @click="
+              () => commentToggleStore.toggleModifyBtn(childComment.commentId)
+            "
+          >
+            수정
+          </button>
+          <CommentDeleteBtn
+            :post-id="childComment.postId"
+            :comment-id="childComment.commentId"
+          />
+        </span>
       </span>
     </li>
   </ul>
