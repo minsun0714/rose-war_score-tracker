69f3fad8980e8b7c979155a2b1adc6180c7e50b3
이민선 <woooooooow22@gmail.com>
2024-12-09 20:43:36 +0900

    feat: 자신의 글에서만 수정 삭제 버튼이 보이도록 v-if문 추가

--- frontend/src/components/Board/PostDetail/PostContent.vue
+++ frontend/src/components/Board/PostDetail/PostContent.vue
@@ -4,7 +4,10 @@ import PostApiFacade from '@/api/apiFacade/PostApiFacade'
 import { format } from 'date-fns'
 import ProfileImg from '@/assets/Male User.svg'
 import PostLikeBtn from './PostLikeBtn.vue'
+import PostDeleteBtn from '../PostDeleteBtn.vue'
+import AuthApiFacade from '@/api/apiFacade/AuthApiFacade'
 
+const { data: userInfo } = AuthApiFacade.useFetchUserInfo()
 const route = useRoute()
 const id = Number(route.params.id)
 const { data: postData } = PostApiFacade.useFetchPost(Number(id))
@@ -12,6 +15,13 @@ const { data: postData } = PostApiFacade.useFetchPost(Number(id))
 
 <template>
   <h1 class="border-b font-bold">{{ postData?.title }}</h1>
+  <div
+    class="flex justify-end text-gray-500 text-sm gap-1 pt-3"
+    v-if="userInfo?.userId === postData?.writer.userId"
+  >
+    <button>수정</button>
+    <PostDeleteBtn :post-id="postData?.postId" />
+  </div>
   <div class="flex justify-between items-center text-xs text-slate-400 py-4">
     <span class="flex flex-row items-center">
       <img :src="postData?.writer.profileImg || ProfileImg" alt="" />
