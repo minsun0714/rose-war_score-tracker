0505e759a1415192c4fb8e99798f9c634c8768c7
이민선 <woooooooow22@gmail.com>
2024-12-14 16:03:13 +0900

    fix: 불필요한 코드 삭제

--- backend/src/main/java/com/rosewar/scoretracker/controller/GameResultController.java
+++ backend/src/main/java/com/rosewar/scoretracker/controller/GameResultController.java
@@ -3,23 +3,24 @@ package com.rosewar.scoretracker.controller;
 import com.rosewar.scoretracker.dto.request.GameResultRequestDTO;
 import com.rosewar.scoretracker.dto.response.GameResultResponseDTO;
 import com.rosewar.scoretracker.service.GameService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+
 @RestController
 @RequestMapping("/api/games")
+@RequiredArgsConstructor
 public class GameResultController {
 
     private final GameService gameService;
 
-    public GameResultController(GameService gameService) {
-        this.gameService = gameService;
-    }
-
     // 게임 결과 생성
     @PostMapping
     public ResponseEntity<GameResultResponseDTO> createGameResult(@RequestBody GameResultRequestDTO gameRequestDTO) {
+        System.out.println(Arrays.deepToString(gameRequestDTO.getGameBoard()));
         GameResultResponseDTO createdGameResult = gameService.createResult(gameRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdGameResult);
     }
