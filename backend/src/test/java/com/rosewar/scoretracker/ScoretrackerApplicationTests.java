382aed358f7ea6c14279ca4a62e36cd672f4bfe5
이민선 <woooooooow22@gmail.com>
2025-01-30 23:28:26 +0900

    fix: 테스트 시 application-test-properties 사용하도록 하는 설정 중복 제거

--- backend/src/test/java/com/rosewar/scoretracker/ScoretrackerApplicationTests.java
+++ backend/src/test/java/com/rosewar/scoretracker/ScoretrackerApplicationTests.java
@@ -3,11 +3,9 @@ package com.rosewar.scoretracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(locations = "classpath:application-test.properties")
 class ScoretrackerApplicationTests {
 
 	@Test
